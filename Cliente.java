729cfcd15d47977204a2254daba5dc46ba48cd13
Daniel Junio <devs.danieljunio@gmail.com>
2024-04-12 10:09:18 -0300

    forms

---
+++ Cliente.java
@@ -0,0 +1,164 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package forms;
+
+/**
+ *
+ * @author conta
+ */
+public class Cliente {
+    
+    private int ID;
+    private String Nome;
+    private String Ender;
+    private String Mun;
+    private String Cep;
+    private String Tel;
+    private String Cel;
+    private String Cpf;
+    private String Cnpj;
+    private String Gen;
+
+    /**
+     * @return the ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * @return the Nome
+     */
+    public String getNome() {
+        return Nome;
+    }
+
+    /**
+     * @param Nome the Nome to set
+     */
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    /**
+     * @return the Ender
+     */
+    public String getEnder() {
+        return Ender;
+    }
+
+    /**
+     * @param Ender the Ender to set
+     */
+    public void setEnder(String Ender) {
+        this.Ender = Ender;
+    }
+
+    /**
+     * @return the Mun
+     */
+    public String getMun() {
+        return Mun;
+    }
+
+    /**
+     * @param Mun the Mun to set
+     */
+    public void setMun(String Mun) {
+        this.Mun = Mun;
+    }
+
+    /**
+     * @return the Cep
+     */
+    public String getCep() {
+        return Cep;
+    }
+
+    /**
+     * @param Cep the Cep to set
+     */
+    public void setCep(String Cep) {
+        this.Cep = Cep;
+    }
+
+    /**
+     * @return the Tel
+     */
+    public String getTel() {
+        return Tel;
+    }
+
+    /**
+     * @param Tel the Tel to set
+     */
+    public void setTel(String Tel) {
+        this.Tel = Tel;
+    }
+
+    /**
+     * @return the Cel
+     */
+    public String getCel() {
+        return Cel;
+    }
+
+    /**
+     * @param Cel the Cel to set
+     */
+    public void setCel(String Cel) {
+        this.Cel = Cel;
+    }
+
+    /**
+     * @return the Cpf
+     */
+    public String getCpf() {
+        return Cpf;
+    }
+
+    /**
+     * @param Cpf the Cpf to set
+     */
+    public void setCpf(String Cpf) {
+        this.Cpf = Cpf;
+    }
+
+    /**
+     * @return the Cnpj
+     */
+    public String getCnpj() {
+        return Cnpj;
+    }
+
+    /**
+     * @param Cnpj the Cnpj to set
+     */
+    public void setCnpj(String Cnpj) {
+        this.Cnpj = Cnpj;
+    }
+
+    /**
+     * @return the Gen
+     */
+    public String getGen() {
+        return Gen;
+    }
+
+    /**
+     * @param Gen the Gen to set
+     */
+    public void setGen(String Gen) {
+        this.Gen = Gen;
+    }
+    
+}
